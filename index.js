77ae0cb34124c1b1f01654cc3fa6e338b85309f5
SonicBuster3098 <140475727+SonicBuster3098@users.noreply.github.com>
2024-11-19 21:50:29 -0800

    skbidi

--- index.js
+++ index.js
@@ -5,28 +5,28 @@ const passbox = document.getElementById("password")
 
 function sendToAPI(){
 
-    // fetch("http://localhost:8080/api/logininfo", {
-    //     method: "post",
-    //     body: JSON.stringify({
-    //         username: loginbox.value,
-    //         password: passbox.value
-    //     }),
-    //     headers: {
-    //         "Content-type": "application/json; charset=UTF-8"
-    //     }
-    // });
-
-    // alert("New Account Created, Check API");
-
-    fetch("http://127.0.0.1:5500/api/testing/1", {
-        method: "get",
+    fetch("http://localhost:8080/api/logininfo", {
+        method: "post",
+        body: JSON.stringify({
+            username: loginbox.value,
+            password: passbox.value
+        }),
         headers: {
             "Content-type": "application/json; charset=UTF-8"
         }
-    })
-    .then(res => res.json())
-    .then(res => {
-        console.log(res);
-    })
+    });
+
+    alert("New Account Created, Check API");
+
+    // fetch("http://127.0.0.1:5500/api/testing/1", {
+    //     method: "get",
+    //     headers: {
+    //         "Content-type": "application/json; charset=UTF-8"
+    //     }
+    // })
+    // .then(res => res.json())
+    // .then(res => {
+    //     console.log(res);
+    // })
     
 }
