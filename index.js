f1931c74616fc2a7ce10758fab2ffdffa944af26
SonicBuster3098 <140475727+SonicBuster3098@users.noreply.github.com>
2024-11-20 16:42:56 -0800

    OMG FIRST POST REQUEST SUIIIII

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-const loginbox = document.getElementById("login")
-const passbox = document.getElementById("password")
+const loginbox = document.getElementById("login");
+const passbox = document.getElementById("password");
 
 
 
@@ -7,16 +7,17 @@ function sendToAPI(){
 
     fetch("http://localhost:8080/api/logininfo", {
         method: "post",
-        body: JSON.stringify({
-            username: loginbox.value,
-            password: passbox.value
-        }),
         headers: {
-            "Content-type": "application/json; charset=UTF-8"
-        }
+            'Content-type': 'application/json;charset=UTF-8'
+        },
+        body: JSON.stringify({
+            "username": loginbox.value,
+            "password": passbox.value
+        })
     });
 
-    alert("New Account Created, Check API");
+    
+    alert("New Account Created -> ");
 
     // fetch("http://127.0.0.1:5500/api/testing/1", {
     //     method: "get",
