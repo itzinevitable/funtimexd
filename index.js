7722c8ba676ae46c90ba928d5067e17f713d0c58
SonicBuster3098 <140475727+SonicBuster3098@users.noreply.github.com>
2024-11-22 16:41:32 -0800

    crash cases

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const loginbox = document.getElementById("login");
 const passbox = document.getElementById("password");
+const recieverbox = document.getElementById("person");
+const msgbox = document.getElementById("msg")
 
 //for chat thingy
 // const el = document.createElement('li');
@@ -8,7 +10,7 @@ const passbox = document.getElementById("password");
 
 
 function createAccount(){
-    fetch("http://localhost:8080/api/logininfo", {
+    fetch("http://localhost:8080/api/login/register", {
         method: "post",
         headers: {
             'Content-type': 'application/json;charset=UTF-8'
@@ -18,16 +20,34 @@ function createAccount(){
             "password": passbox.value
         })
     })
-    .then(response => response.text())
+    .then(response => {
+        if(!response.ok){
+            throw new Error("Server is down")
+        }
+        return response.text();
+    })
     .then((dataStr) =>{
         if(dataStr == "User already existent"){
             alert("Username taken, try again.")
         }
+    })
+    .catch((error) =>{
+        alert("Server is down.")
     });
     
-    alert("New Account Created -> ");
 }
 
 function sendMessage(){
+    if(recieverbox.value == "" || msgbox.value == ""){
+        alert("Please fill out both boxes before sending a message.")
+    }
 
+    fetch("http://localhost:8080/api/msg/send"{
+        method: "post",
+        headers: "application/json;charset=UTF-8",
+        body: JSON.stringify({
+
+        })
+
+    })
 }
