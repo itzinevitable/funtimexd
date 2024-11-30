af6b459dddcd2f5dbcfb6fbee5cac4bae4fa9815
SonicBuster3098 <140475727+SonicBuster3098@users.noreply.github.com>
2024-11-30 22:16:45 +0800

    j

--- data-specific/index.js
+++ data-specific/index.js
@@ -1,23 +1,37 @@
-const loginbox = document.getElementById("login");
-const passbox = document.getElementById("password");
 const recieverbox = document.getElementById("person");
-const msgbox = document.getElementById("msg")
+const msgbox = document.getElementById("msg");
 
 //for chat thingy
 // const el = document.createElement('li');
 // el.innerHTML = text;
 // document.querySelector('ul').appendChild(el)
 
+var client;
+var loggedIn;
+
+while(true){
+    while(loggedIn){
+        console.log("logged in")
+    }
+}
+
+function Client(username, password){
+    this.username = username
+    this.password = password
+}
 
 function createAccount(){
+    const username = document.getElementById("username")
+    const password = document.getElementById("password")
+
     fetch("http://localhost:8080/api/login/register", {
         method: "post",
         headers: {
             'Content-type': 'application/json;charset=UTF-8'
         },
         body: JSON.stringify({
-            "username": loginbox.value,
-            "password": passbox.value
+            "username": username.value,
+            "password": password.value
         })
     })
     .then(response => {
@@ -37,17 +51,74 @@ function createAccount(){
     
 }
 
+function login(){
+    const username = document.getElementById("login")
+    const password = document.getElementById("pass")
+
+    fetch("http://localhost:8080/api/login/signin",{
+        method: "post",
+        headers: {
+            'Content-type': "application/json;charset=UTF-8"
+        },
+        body: JSON.stringify({
+            username : username.value,
+            password : password.value
+        })
+    })
+    .then(response =>{
+        if(!response.ok){
+            throw new Error("Server is down");
+        }
+        return response.text();
+    })
+    .then((responseText) =>{
+        if(responseText == "Welcome!"){
+            client = new Client(username.value, password.value)
+            username.value = "";
+            password.value = "";
+            loggedIn = true
+            alert("logged in")
+        }else{
+            alert("Incorrect Password. Try again")
+            username.value = "";
+            password.value = "";
+        }
+    })
+    .catch((error) =>{
+        alert(error)
+    });
+
+   
+}
+
 function sendMessage(){
+    if(loggedIn == null){
+        alert("Please log in.")
+        return
+    }
+
     if(recieverbox.value == "" || msgbox.value == ""){
         alert("Please fill out both boxes before sending a message.")
+        return
     }
 
     fetch("http://localhost:8080/api/msg/send", {
         method: "post",
-        headers: "application/json;charset=UTF-8",
+        headers: {
+            "content-type": "application/json;charset=UTF-8"
+        },
         body: JSON.stringify({
-            
+            username: client.username,
+            message: document.getElementById("msg").value,
+            reciever: document.getElementById("person").value
         })
-
+    })
+    .then(response =>{
+        if(!response.ok){
+            throw new Error("Server is down")
+        }
+    })
+    .catch((error) =>{
+        alert(error)
     })
 }
